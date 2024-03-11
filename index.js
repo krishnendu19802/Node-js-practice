6f4b73e92f8eaff2dbae80087d0224e752103588
Krishnendu Bhattacharyya <krishnendu19802@gmail.com>
2024-03-11 13:05:31 +0530

    new file

--- index.js
+++ index.js
@@ -1,75 +1,83 @@
-const express=require('express')
-const app=express()
-const port =5000
-const Joi=require('joi')
+const express = require('express')
+const app = express()
+const port = 5000
+const Joi = require('joi')
+const ProductRouter=require('./products.routes.js')
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+const mongoose = require('mongoose')
+const { MongoClient } = require('mongodb');
+
+const Product= require('./Models/product.model.js')
 // Middleware to parse URL-encoded form data in the request body
 // app.use(express.urlencoded({ extended: true }));
 
+// mongoose.connect('mongodb+srv://krishnendu19802:Fx8uunIHaJJkorOW@cluster0.4lahfo1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+//   console.log('Connected successfully')
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
+// }).catch(() => { console.log('error connecting') })
 
-let arr=[
-  {id:1,name:'course1'},
-  {id:2,name:'course2'},
-  {id:3,name:'course3'},
 
-]
-
-app.get('/', (req, res) => {
-    res.send(arr)
-  })
-
-app.get('/:id',(req,res)=>{
-  const params=req.params.id
-  console.log(params)
-  res.send(arr[params])
-})
-
-function check(obj){
-  const schema=Joi.object({
-    name: Joi.string().min(4)
-  })
-  // console.log(req.body)
-  return schema.validate(obj)
-}
-
-app.post('/newcontent',(req,res)=>{
-  const {error,value}=check(req.body)
-  
-  if(error){
-    console.log(error)
-    res.status(400).send('Wrong value received')
-    return
-  }
-  else{
-    arr.push({id:arr.length+1,name:req.body.name})
-    res.send(arr)
-  }
-  
-})
-
-app.put('/:id',(req,res)=>{
-  const ind=parseInt(req.params.id)
-  let elem=arr.find(each=> each.id===ind)
-  if(!elem ){
-    res.status(404).send('No such value')
-    return
-  }
-  const body=req.body
-  const {error,value}=check(body)
-  if(error){
-    console.log(error)
-    res.status(400).send('Wrong value received')
-    return
-  }
-  else{
-    elem.name=body.name
-    res.send(arr)
-    
-  }
-  
-})
- 
+mongoose.connect('mongodb://127.0.0.1:27017/learning?directConnection=true').then(() => {
+  console.log('Connected successfully')
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+}).catch((err) => { console.log('error connecting',err) })
+
+let arr = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+
+]
+app.use('/',ProductRouter)
+
+
+// function check(obj) {
+//   const schema = Joi.object({
+//     name: Joi.string().min(4)
+//   })
+//   // console.log(req.body)
+//   return schema.validate(obj)
+// }
+
+// app.post('/newcontent', async(req, res) => {
+//  
+//   // if (error) {
+//   //   console.log(error)
+//   //   res.status(400).send('Wrong value received')
+//   //   return
+//   // }
+//   // else {
+//   //   arr.push({ id: arr.length + 1, name: req.body.name })
+//   //   res.send(arr)
+//   // }
+
+// })
+
+// app.put('/:id', async(req, res) => {
+//   // const ind = parseInt(req.params.id)
+//   // let elem = arr.find(each => each.id === ind)
+//   // if (!elem) {
+//   //   res.status(404).send('No such value')
+//   //   return
+//   // }
+//   // const body = req.body
+//   // const { error, value } = check(body)
+//   // if (error) {
+//   //   console.log(error)
+//   //   res.status(400).send('Wrong value received')
+//   //   return
+//   // }
+//   // else {
+//   //   elem.name = body.name
+//   //   res.send(arr)
+
+//   // }
+
+// })
+
